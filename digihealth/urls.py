efbe6680d6de97af94eba931421764a642c35078
TF758 <teryllfelix@gmail.com>
2023-11-19 00:35:35 -0400

    minor refactoring
    - moved auth urls to own file

--- digihealth/urls.py
+++ digihealth/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('healthapp.urls'),),
+    path('auth/', include('healthapp.auth_urls')),
     # path('api/', include('healthapp.api_urls'),)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ healthapp/auth_urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path, include, register_converter
+from healthapp.views import *
+
+from . import views
+
+
+urlpatterns = [
+    path("", AdminDashBoard.as_view(), name='new-center'),
+    path("add-center", CreateNewCenter.as_view(), name='new-center'),
+    path("manage-centers", ManageCenters.as_view(), name='manage-centers'),
+    path("center/delete/<int:pk>", DeleteCenter.as_view(), name='delete-center'),
+    path("center/edit/<int:pk>", UpdateCenter.as_view(), name='update-center'),
+    path("add-clinic", CreateNewClinicEvent.as_view(), name='new-clinic'),
+    path("manage-clinics", ManageClinics.as_view(), name='manage-clinics'),
+    path("clinic/delete/<int:pk>", DeleteClinic.as_view(), name='delete-clinic'),
+    path("clinic/edit/<int:pk>", UpdateClinic.as_view(), name='update-clinic'),
+]
